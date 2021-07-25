9e68464ad4adfef418ab2d351baac9070a014c07
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-07-24 19:06:51 -0600

    Add specs for /sessions route

--- backend/spec/requests/sessions_spec.rb
+++ backend/spec/requests/sessions_spec.rb
@@ -1,7 +1,112 @@
 require 'rails_helper'
 
 RSpec.describe "Sessions", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  # init test data
+  let!(:sessions) { create_list(:session, 10) }
+  let(:session_id) { sessions.first.id }
+
+  # Test suite for GET /sessions
+  describe "GET /sessions" dp
+    before { get '/sessions' }
+
+    it 'returns sessions' do
+      expect(json).not_to be_empty
+      expect(json.size).to eq(10)
+    end
+
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
+    end
   end
+
+  # Test suite for GET /sessions/:id
+  describe
+    before { get "/sessions/#{session_id}" }
+
+    context 'when the session exists' do
+      it 'returns the session' do
+        expect(json).not_to be_empty
+        expect(json['session_title']).to eq(sessions.first.session_title)
+      end
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context 'when the user does not exist' do
+      let(:session_id) { -1 }
+
+      it 'returns status code 404' do
+        expect(response).to have_http_status(200)
+      end
+
+      it 'returns a not found message' do
+        expect(response.body).to match(/Couldn't find Session/)
+      end
+    end
+  end
+
+  # Test suire for POST /sessions
+  describe 'POST /sessions' do
+    context 'when the request is valid' do
+      session = { 
+        user_id: 1, 
+        session_title: 'example title',
+        spots_available: 10, 
+        total_spots: 1, 
+        start_date: '2021-07-21 ', 
+        start_time: '2021-07-22 12:30:59 -0700', 
+        end_time: '2021-07-22 1:30:59 -0700', 
+      }
+        before { post '/sessions', params { session } }
+
+        it 'creates a new session' do
+          expect(json['session_title']).to eq('example title')
+        end
+
+        it 'returns status code 200' do
+          expect(response).to have_http_status(200);
+        end
+      end
+    end
+
+    context 'when the request is invalid' do
+      before { post '/sessions', params: { session: nil } }
+
+      it 'returns a status code 401' do
+       expect(response).to have_http_status(401)
+      end
+
+      it 'returns a validation failure message' do 
+        expect(response.body).to match(/Error: /)
+      end
+    end
+  end
+
+  # Test suire for PUT /sessions/:id
+  describe 'PUT /sessions/:id' do
+    context 'when the session exists' do
+      before { put "/sessions/#{session_id}", params: { session_title: 'exampleTest' } }
+
+      it 'updates the session' do 
+        expect(json).not_to be_empty
+        expect(json['username']).to eq('exampleTest')
+      end
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+  end
+
+  # Test suite for DELETE /sessions/:id
+  describe
+    before { delete "/sessions/#{session_id}" }
+
+    it 'returns status code 204' do
+      expect(response).to have_http_status(204)
+    end
+  end
+
 end
