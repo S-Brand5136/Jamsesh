ff8b07cf7000d50350ad622930501a71c77eeb45
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-07-27 17:27:31 -0600

    Update users_in_session factory add associations

--- backend/spec/factories/users_in_sessions.rb
+++ backend/spec/factories/users_in_sessions.rb
@@ -1,6 +1,8 @@
 FactoryBot.define do
   factory :users_in_session do
-    user_id { Faker::Number.number(10) }
-    session_id { Faker::Number.number(10) }
+    user_id { Faker::Number.number(digits: 2) }
+    session_id { Faker::Number.number(digits: 2) }
+    association :user, factory: :user
+    association :session, factory: :session
   end
 end
