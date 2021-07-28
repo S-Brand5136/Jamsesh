19a7d08e1ff5adf97526c29d0b3243750b72cb99
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-07-27 18:23:00 -0600

    Set up react-router-dom base routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-function App() {
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+
+const App = () => {
   return (
     <div className="App">
-
+      <Header />
+      <Router>
+        <Route path='/' component={HomePage} exact />
+      </Router>
+      <Footer />
     </div>
   );
 }
