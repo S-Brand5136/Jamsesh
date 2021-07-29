439844034cad3f5592d082460b8041b3eb96c8b5
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-07-28 19:21:01 -0600

    Refactor component directories

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,8 @@
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Header from './components/global/Header';
+import Footer from './components/global/Footer';
 import HomePage from './pages/HomePage';
 
 const App = () => {
