e00d36bb10e2dda857655c4cdd65f9abf9b1c17c
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-08-02 19:36:10 -0600

    Add Login and Register pages to app router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/navbar';
 import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 const App = () => {
   return (
@@ -11,6 +13,8 @@ const App = () => {
       <Router>
         <Header />
         <Route path='/' component={HomePage} exact />
+        <Route path='/login' component={Login} exact />
+        <Route path='/register' component={Register} exact />
         <Footer />
       </Router>
     </div>
