b72f1ff6300ffdcbac92fcdc7ff5ba2ecfcd6e82
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-08-04 18:10:49 -0600

    Add theme and themeProvider wrapper

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import './styles/App.scss';
+import { ThemeProvider } from '@material-ui/core';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+import theme from './themes/theme';
 import Header from './components/navbar';
 import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
@@ -9,15 +11,17 @@ import Register from './pages/Register';
 
 const App = () => {
   return (
-    <div className='App'>
-      <Router>
-        <Header />
-        <Route path='/' component={HomePage} exact />
-        <Route path='/login' component={Login} exact />
-        <Route path='/register' component={Register} exact />
-        <Footer />
-      </Router>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className='App'>
+        <Router>
+          <Header />
+          <Route path='/' component={HomePage} exact />
+          <Route path='/login' component={Login} exact />
+          <Route path='/register' component={Register} exact />
+          <Footer />
+        </Router>
+      </div>
+    </ThemeProvider>
   );
 };
 
